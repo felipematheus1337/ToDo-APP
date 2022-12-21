6b3d0ee59f22ac5463eb84ac775a35d24f8a32da
felipematheus1337 <lipehbr@gmail.com>
2022-12-21 15:28:00 -0300

    type item created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import * as C from "./App.styles";
+import { useState } from 'react';
 
 const App = () => {
+
+  const [list, setList] = useState([]);
+
   return (
     <C.Container>
       <C.Area>
